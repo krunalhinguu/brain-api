cacaaf544807accb71eb691a363ab40da47b5763
krunal hingu <krunal.hingu222@gmail.com>
2020-06-12 19:45:16 +0530

    updated db host

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const users = require("./controllers/users");
 const db = knex({
   client: "pg",
   connection: {
-    host: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
     ssl: true,
   },
 });
